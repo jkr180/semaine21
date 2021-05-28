ec4cedeeee5e789f623d4c42cf1e01790e38932d
Lucas <lucaslocatin180@gmail.com>
2021-05-28 12:25:05 +0200

    message

---
+++ PersonnageDefault.class.php
@@ -0,0 +1,14 @@
+<?php
+class PersonnageDefault
+{
+// Attributs
+public $nom ='Loper';
+public $prenom = 'Dave';
+public $age= '18 ans';
+public $sexe = 'masculin';
+
+public function __construct() {}
+
+
+// Méthodes
+}
